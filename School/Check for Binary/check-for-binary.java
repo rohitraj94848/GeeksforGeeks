f0a6dae822d40a9ae1f30ad7847f6bf6d28834fe
rohitraj94848 <138149999+rohitraj94848@users.noreply.github.com>
2024-01-01 15:51:47 +0530

    Added Solution - GfG to GitHub

---
+++ School/Check for Binary/check-for-binary.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+import java.util.*;
+
+
+class checkBinary
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		sc.nextLine();
+		while(T>0)
+		{
+			String str = sc.nextLine();
+			GfG g = new GfG();
+			boolean b = g.isBinary(str);
+			if(b== true)
+				System.out.println(1);
+			else
+			    System.out.println(0);
+		T--;
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class GfG
+{
+	boolean isBinary(String str)
+	{
+        int length = str.length();
+
+        for (int i = 0; i < length; i++) {
+            char currentChar = str.charAt(i);
+            if (currentChar != '0' && currentChar != '1') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
