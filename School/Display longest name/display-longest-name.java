b393370b6c4882c836321ae8192ace951f803f8e
rohitraj94848 <138149999+rohitraj94848@users.noreply.github.com>
2024-01-02 22:21:13 +0530

    Added Solution - GfG to GitHub

---
+++ School/Display longest name/display-longest-name.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.util.Scanner;
+
+  public class longestString {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t;
+
+        t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n;
+            n = sc.nextInt();
+            String[] names = new String[n];
+
+            for (int j = 0; j < n; j++) names[j] = sc.next();
+
+            GFG obj = new GFG();
+            System.out.println(obj.longest(names, n));
+        }
+    }
+
+}
+
+// } Driver Code Ends
+
+
+class GFG {
+    String longest(String names[], int n) {
+        int index = 0, max = 0;
+        for(int i = 0 ; i < n  ; i++){
+            int l = names[i].length();
+             
+            if(l > max){
+                max = l ;
+                index = i;
+            }
+            
+        }
+        return names[index];
+    }
+}
