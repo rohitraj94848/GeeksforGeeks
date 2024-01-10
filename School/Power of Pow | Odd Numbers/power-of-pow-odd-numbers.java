e01263821ee8ff405d1ae1a9f1b64d96ca0b9fc5
rohitraj94848 <138149999+rohitraj94848@users.noreply.github.com>
2024-01-10 19:06:17 +0530

    Added Solution - GfG to GitHub

---
+++ School/Power of Pow | Odd Numbers/power-of-pow-odd-numbers.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());            
+            Solution ob = new Solution();
+            long ans = ob.sum_of_square_oddNumbers(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long sum_of_square_oddNumbers(long n)
+    {
+        // Code here
+        long ans = 0;
+        int odd = 1;
+        for(int i =1; i <= n ; i++)
+        {
+         ans = ans + (odd*odd);  
+         odd = odd + 2;
+        }
+        return ans;
+    }
+}
