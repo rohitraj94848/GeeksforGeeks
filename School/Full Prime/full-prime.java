3b34221a6977ce465a52984948242b898a6d64ac
rohitraj94848 <138149999+rohitraj94848@users.noreply.github.com>
2024-01-11 21:12:09 +0530

    Added Solution - GfG to GitHub

---
+++ School/Full Prime/full-prime.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.fullPrime(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int fullPrime(int N) {
+       
+        if (!isPrime(N)) {
+            return 0;
+        }
+
+       
+        while (N > 0) {
+            int digit = N % 10;
+            if (!isPrime(digit)) {
+                return 0;
+            }
+            N /= 10;
+        }
+
+        return 1;
+    }
+
+    
+    public static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
