15ed8d361c4ca8aabdfd80da62b10b9c826ec55d
rohitraj94848 <138149999+rohitraj94848@users.noreply.github.com>
2024-01-12 22:57:07 +0530

    Added Solution - GfG to GitHub

---
+++ School/Sum of an AP/sum-of-an-ap.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            int n = Integer.parseInt(S[0]);
+            int a = Integer.parseInt(S[1]);
+            int d = Integer.parseInt(S[2]);
+            Solution ob = new Solution();
+            int ans = ob.sum_of_ap(n, a, d);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int sum_of_ap(int n, int a, int d)
+    {
+        // Code here
+         int sum = 0;
+              
+              for(int i=0; i<n; i++){
+                  
+                  sum += a+i*d;
+              }
+              
+              return sum;
+              
+    }
+}
